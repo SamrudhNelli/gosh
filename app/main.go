0c438508dc513a87ddb2a2ae01fd850f0871b976
Samrudh Nelli <samrudhnelli@gmail.com>
2026-01-18 19:05:51 +0530

    Implement type and use strings.Fields to rectify echo

--- app/main.go
+++ app/main.go
@@ -1,30 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
-	"unicode"
+	"strings"
 )
 
-func main() {   
+func main() {
 	for {
 		fmt.Print("$ ")
-		command, error := bufio.NewReader(os.Stdin).ReadString('\n')
+		rawCommand, error := bufio.NewReader(os.Stdin).ReadString('\n')
+		command := strings.Fields(rawCommand)
+
 		if error != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", error)
 			os.Exit(1)
 		}
-		if command == "exit\n" { 
-			break 
-		} else if command[:4] == "echo" && unicode.IsSpace(rune(command[4])) {
-			if len(command) == 5 {
+
+		if command[0] == "exit" {
+			break
+		} else if command[0] == "echo" {
+			if len(command) == 1 {
 				fmt.Println()
 			} else {
-				fmt.Print(command[5:len(command)])
+				for i := 1; i < len(command); i++ {
+					fmt.Print(command[i] + " ")
+				}
+				fmt.Println()
+			}
+		} else if command[0] == "type" {
+			if len(command) == 1 {
+				fmt.Print()
+			} else {
+				for i := 1; i < len(command); i++ {
+					if command[i] == "echo" || command[i] == "exit" || command[i] == "type" {
+						fmt.Println(command[i] + " is a shell builtin")
+					} else {
+						fmt.Println(command[i] + ": not found")
+					}
+				}
 			}
 		} else {
-			fmt.Println(command[0:(len(command) - 1)] + ": command not found")
+			fmt.Println(command[0] + ": command not found")
 		}
 	}
 }
