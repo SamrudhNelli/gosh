d505c923473f11880d9b69abd01c71e05c4cd6de
Samrudh Nelli <samrudhnelli@gmail.com>
2026-01-18 17:41:14 +0530

    Push solution of handle invalid commands

--- app/main.go
+++ app/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+	command, error := bufio.NewReader(os.Stdin).ReadString('\n')
+	var _ = error
+	fmt.Println(command[0:(len(command) - 1)] + ": command not found")
 }
