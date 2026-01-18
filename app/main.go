b70385d1d0c81b7da96a851eb8d1bee5ec2193c2
Samrudh Nelli <samrudhnelli@gmail.com>
2026-01-18 18:21:18 +0530

    Implement echo

--- app/main.go
+++ app/main.go
@@ -4,14 +4,27 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"unicode"
 )
 
 func main() {   
 	for {
 		fmt.Print("$ ")
 		command, error := bufio.NewReader(os.Stdin).ReadString('\n')
-		var _ = error
-		if command == "exit\n" { break }
-		fmt.Println(command[0:(len(command) - 1)] + ": command not found")
+		if error != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", error)
+			os.Exit(1)
+		}
+		if command == "exit\n" { 
+			break 
+		} else if command[:4] == "echo" && unicode.IsSpace(rune(command[4])) {
+			if len(command) == 5 {
+				fmt.Println()
+			} else {
+				fmt.Print(command[5:len(command)])
+			}
+		} else {
+			fmt.Println(command[0:(len(command) - 1)] + ": command not found")
+		}
 	}
 }
