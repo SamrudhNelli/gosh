d82b9fbec8f2e78dc816d5ef7c1c46259e329d58
Samrudh Nelli <samrudhnelli@gmail.com>
2026-01-20 01:32:12 +0530

    feat: implement cd

--- app/main.go
+++ app/main.go
@@ -76,6 +76,27 @@ func main() {
 				log.Fatal(error)
 			}
 			fmt.Println(path)
+		} else if command[0] == "cd" {
+			if len(command) == 1 || command[1] == "~" {
+				home, error := os.UserHomeDir()
+				if error != nil {
+					log.Fatal(error)
+				}
+				error = os.Chdir(home)
+				if error != nil {
+					log.Fatal(error)
+				}
+			} else {
+				fileInfo, error := os.Stat(command[1])
+				if error == nil && fileInfo.IsDir() {
+					error = os.Chdir(command[1])
+					if error != nil {
+						log.Fatal(error)
+					}
+				} else {
+					fmt.Printf("cd: %s: No such file or directory\n", command[1])
+				}
+			}
 		} else if foundExec, fullPath := findExec(command[0]); foundExec {
 			cmd := exec.Command(command[0], command[1:]...)
 			cmd.Stdout = os.Stdout
