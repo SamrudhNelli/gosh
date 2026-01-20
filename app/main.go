2cc852416bcfec3b8851c09f126d2c371460d601
Samrudh Nelli <samrudhnelli@gmail.com>
2026-01-20 23:20:41 +0530

    feat: implement 1>> and 2>> support for appending to files without truncation

--- app/main.go
+++ app/main.go
@@ -111,34 +111,55 @@ func isExec(command []string) (bool) {
 	return foundExec
 }
 
-func RunExec(command []string) {
+func returnExec(command []string) (cmd *exec.Cmd, outFile *os.File) {
 	index, flag := checkRedirectRequest(command)
-	var cmd *exec.Cmd
-
+	var error error
 	if flag == 1 {
-		outFile, error := os.Create(command[index + 1])
+		outFile, error = os.Create(command[index + 1])
 		if error != nil {
 			log.Fatal(error)
 		}
-		defer outFile.Close()
 		cmd = exec.Command(command[0], command[1:index]...)
 		cmd.Stdout = outFile
 		cmd.Stderr = os.Stderr
 	} else if flag == 2 {
-		outFile, error := os.Create(command[index + 1])
+		outFile, error = os.Create(command[index + 1])
 		if error != nil {
 			log.Fatal(error)
 		}
-		defer outFile.Close()
 		cmd = exec.Command(command[0], command[1:index]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = outFile
+	} else if flag == 3 {
+		outFile, error = os.OpenFile(command[index + 1], os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+		if error != nil {
+			log.Fatal(error)
+		}
+		cmd = exec.Command(command[0], command[1:index]...)
+		cmd.Stdout = outFile
+		cmd.Stderr = os.Stderr
+	} else if flag == 4 {
+		outFile, error = os.OpenFile(command[index + 1], os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+		if error != nil {
+			log.Fatal(error)
+		}
+		cmd = exec.Command(command[0], command[1:index]...)
+		cmd.Stderr = outFile
+		cmd.Stdout = os.Stdout
 	} else {
 		cmd = exec.Command(command[0], command[1:]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+		return cmd, nil
 	}
+	return
+}
 
+func RunExec(command []string) {
+	cmd, outFile := returnExec(command)
+	if outFile != nil {
+		defer outFile.Close()
+	}
 	error := cmd.Run()
 	// if error != nil {
 	// 	// _, fullPath := findExec(command[0])
@@ -150,39 +171,64 @@ func RunExec(command []string) {
 
 func checkRedirectRequest(command []string) (int, int) {
 	index := slices.IndexFunc(command, func(c string) bool {
-		return c == "1>" || c == ">" || c == "2>"
+		return c == "1>" || c == ">" || c == "2>" || c == "1>>" || c == ">>" || c == "2>>"
 	})
 	if index == -1 {
 		return index, -1
 	} else if command[index] == "2>" {
 		return index, 2
-	} else {
+	} else if command[index] == "2>>" {
+		return index, 4
+	} else if command[index] == "1>" || command[index] == ">" {
 		return index, 1
+	} else {
+		return index, 3
 	}
 }
 
 func redirectEchoToFile(command []string, index int, print string, flag int) (string) {
+	if len(command) == index + 1 {
+		fmt.Println("No output file specified!!")
+		return ""
+	}
+
 	if flag == 1 {
-		if len(command) == index + 1 {
-			fmt.Println("No output file specified!!")
-			return ""
-		}
 		data := []byte(print + strings.Join(command[min(index + 2, len(command)):], " "))
 		error := os.WriteFile(command[index + 1], data, 0644)
 		if error != nil {
 			log.Fatal(error)
 		}
-	} else {
-		if len(command) == index + 1 {
-			fmt.Println("No output file specified!!")
-			return ""
-		}
+	} else if flag == 2 {
 		data := []byte("")
 		error := os.WriteFile(command[index + 1], data, 0644)
 		if error != nil {
 			log.Fatal(error)
 		}
 		return print
+	} else if flag == 3 {
+		data := []byte(print + strings.Join(command[min(index + 2, len(command)):], " "))
+		file, error := os.OpenFile(command[index + 1], os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+		if error != nil {
+			log.Fatal(error)
+		}
+		defer file.Close()
+		_, error = file.Write(data)
+		if error != nil {
+			file.Close()
+			log.Fatal(error)
+		}
+	} else if flag == 4 {
+		data := []byte("")
+		file, error := os.OpenFile(command[index + 1], os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+		if error != nil {
+			log.Fatal(error)
+		}
+		defer file.Close()
+		_, error = file.Write(data)
+		if error != nil {
+			file.Close()
+			log.Fatal(error)
+		}
 	}
 	return ""
 }
