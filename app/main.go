dd742c3896e3c33878106cf7ea086a6499551d2c
Samrudh Nelli <samrudhnelli@gmail.com>
2026-01-24 01:30:27 +0530

    feat: implement pipelines for two commands

--- app/main.go
+++ app/main.go
@@ -29,16 +29,16 @@ func initCompleters() []readline.PrefixCompleterInterface {
 	path := os.Getenv("PATH")
 	pathSlice := strings.Split(path, ":")
 	for i := 0; i < len(pathSlice); i++ {
-		entries, error := os.ReadDir(pathSlice[i])
-		if error != nil {
+		entries, err := os.ReadDir(pathSlice[i])
+		if err != nil {
 			continue
 		}
 		for j := 0; j < len(entries); j++ {
 			if entries[j].IsDir() || uniqueStrings[entries[j].Name()] {
 				continue
 			}
-			fileInfo, error := entries[j].Info()
-			if error != nil {
+			fileInfo, err := entries[j].Info()
+			if err != nil {
 				continue
 			}
 			if fileInfo.Mode()&0111 != 0 {
@@ -145,29 +145,29 @@ func Type(command []string) (print string) {
 }
 
 func Pwd() string {
-	path, error := os.Getwd()
-	if error != nil {
-		log.Fatal(error)
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
 	}
 	return fmt.Sprintf("%s\n", path)
 }
 
 func Cd(command []string) string {
 	if len(command) == 1 || command[1] == "~" {
-		home, error := os.UserHomeDir()
-		if error != nil {
-			log.Fatal(error)
+		home, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
 		}
-		error = os.Chdir(home)
-		if error != nil {
-			log.Fatal(error)
+		err = os.Chdir(home)
+		if err != nil {
+			log.Fatal(err)
 		}
 	} else {
-		fileInfo, error := os.Stat(command[1])
-		if error == nil && fileInfo.IsDir() {
-			error = os.Chdir(command[1])
-			if error != nil {
-				log.Fatal(error)
+		fileInfo, err := os.Stat(command[1])
+		if err == nil && fileInfo.IsDir() {
+			err = os.Chdir(command[1])
+			if err != nil {
+				log.Fatal(err)
 			}
 		} else {
 			return fmt.Sprintf("cd: %s: No such file or directory\n", command[1])
@@ -188,8 +188,8 @@ func findExec(program string) (bool, string) {
 			fullPath = pathSlice[j] + program
 		}
 
-		fileInfo, error := os.Stat(fullPath)
-		if error == nil {
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil {
 			mode := fileInfo.Mode()
 			if mode&0b001001001 != 0 { // mode is stored as rwxrwxrwx
 				return true, fullPath
@@ -206,35 +206,35 @@ func isExec(command []string) bool {
 
 func returnExec(command []string) (cmd *exec.Cmd, outFile *os.File) {
 	index, flag := checkRedirectRequest(command)
-	var error error
+	var err error
 	if flag == 1 {
-		outFile, error = os.Create(command[index+1])
-		if error != nil {
-			log.Fatal(error)
+		outFile, err = os.Create(command[index+1])
+		if err != nil {
+			log.Fatal(err)
 		}
 		cmd = exec.Command(command[0], command[1:index]...)
 		cmd.Stdout = outFile
 		cmd.Stderr = os.Stderr
 	} else if flag == 2 {
-		outFile, error = os.Create(command[index+1])
-		if error != nil {
-			log.Fatal(error)
+		outFile, err = os.Create(command[index+1])
+		if err != nil {
+			log.Fatal(err)
 		}
 		cmd = exec.Command(command[0], command[1:index]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = outFile
 	} else if flag == 3 {
-		outFile, error = os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if error != nil {
-			log.Fatal(error)
+		outFile, err = os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 		cmd = exec.Command(command[0], command[1:index]...)
 		cmd.Stdout = outFile
 		cmd.Stderr = os.Stderr
 	} else if flag == 4 {
-		outFile, error = os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if error != nil {
-			log.Fatal(error)
+		outFile, err = os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 		cmd = exec.Command(command[0], command[1:index]...)
 		cmd.Stderr = outFile
@@ -253,13 +253,13 @@ func RunExec(command []string) {
 	if outFile != nil {
 		defer outFile.Close()
 	}
-	error := cmd.Run()
-	// if error != nil {
+	err := cmd.Run()
+	// if err != nil {
 	// 	// _, fullPath := findExec(command[0])
 	// 	// fmt.Printf("Something went wrong! Could not execute %s\n", fullPath)
-	// 	// log.Fatal(error)
+	// 	// log.Fatal(err)
 	// }
-	var _ = error
+	var _ = err
 }
 
 func checkRedirectRequest(command []string) (int, int) {
@@ -287,40 +287,40 @@ func redirectEchoToFile(command []string, index int, print string, flag int) str
 
 	if flag == 1 {
 		data := []byte(print + strings.Join(command[min(index+2, len(command)):], " "))
-		error := os.WriteFile(command[index+1], data, 0644)
-		if error != nil {
-			log.Fatal(error)
+		err := os.WriteFile(command[index+1], data, 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 	} else if flag == 2 {
 		data := []byte("")
-		error := os.WriteFile(command[index+1], data, 0644)
-		if error != nil {
-			log.Fatal(error)
+		err := os.WriteFile(command[index+1], data, 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 		return print
 	} else if flag == 3 {
 		data := []byte(print + strings.Join(command[min(index+2, len(command)):], " "))
-		file, error := os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if error != nil {
-			log.Fatal(error)
+		file, err := os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 		defer file.Close()
-		_, error = file.Write(data)
-		if error != nil {
+		_, err = file.Write(data)
+		if err != nil {
 			file.Close()
-			log.Fatal(error)
+			log.Fatal(err)
 		}
 	} else if flag == 4 {
 		data := []byte("")
-		file, error := os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if error != nil {
-			log.Fatal(error)
+		file, err := os.OpenFile(command[index+1], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 		defer file.Close()
-		_, error = file.Write(data)
-		if error != nil {
+		_, err = file.Write(data)
+		if err != nil {
 			file.Close()
-			log.Fatal(error)
+			log.Fatal(err)
 		}
 		return print
 	}
@@ -358,35 +358,79 @@ func commandParser(rawCommand string) (command []string) {
 	return
 }
 
+func ExecutePipes(command []string) {
+	commands := splitPipes(command)
+	c1 := exec.Command(commands[0][0], commands[0][1:]...)
+	c2 := exec.Command(commands[1][0], commands[1][1:]...)
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c1.Stdin = os.Stdin
+	c2.Stderr = os.Stderr
+	c1.Stderr = os.Stderr
+	c2.Stdout = os.Stdout
+	c1.Stdout = w
+	c2.Stdin = r
+	c1.Start()
+	c2.Start()
+	c1.Wait()
+	w.Close()
+	c2.Wait()
+}
+
+func splitPipes(command []string) (commands [][]string) {
+	var commandSet []string
+	for _, val := range command {
+		if val == "|" {
+			commands = append(commands, commandSet)
+			commandSet = nil		
+		} else {
+			commandSet = append(commandSet, val)
+		}
+	}
+	commands = append(commands, commandSet)
+	return
+}
+
+func hasPipelines(command []string) bool {
+	for _, val := range command {
+		if val == "|" {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 
 	completer := readline.NewPrefixCompleter(initCompleters()...)
 	customCompleter := &bellCompleter{
 		AutoCompleter: completer,
 	}
-	rl, error := readline.NewEx(&readline.Config{
+	rl, err := readline.NewEx(&readline.Config{
 		Prompt:          "$ ",
 		AutoComplete:    customCompleter,
 		InterruptPrompt: "^C",
 		EOFPrompt:       "exit",
 	})
-	if error != nil {
-		log.Fatal(error)
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer rl.Close()
 
 	for {
 
-		rawCommand, error := rl.Readline()
-		if error == readline.ErrInterrupt { // Ctrl + C
+		rawCommand, err := rl.Readline()
+		if err == readline.ErrInterrupt { // Ctrl + C
 			if len(rawCommand) == 0 {
 				break
 			} else {
 				continue
 			}
-		} else if error == io.EOF { // Ctrl + D and other errors
+		} else if err == io.EOF { // Ctrl + D and other errs
 			break
-		} else if error != nil { // Other rare errors! Something broke!!
+		} else if err != nil { // Other rare errors! Something broke!!
 			break
 		}
 
@@ -396,7 +440,9 @@ func main() {
 			continue
 		}
 
-		if command[0] == "exit" {
+		if hasPipelines(command) {
+			ExecutePipes(command)
+		} else if command[0] == "exit" {
 			break
 		} else if command[0] == "echo" {
 			fmt.Print(Echo(command))
